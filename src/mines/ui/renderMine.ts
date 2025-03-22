b7b9f6ec9d7a1129b6b5204ee21218a95d74088a
Ivet Lopez <ivet19@live.com>
2025-03-22 20:02:54 +0100

    Delete repeated declaration of the variable mine as HTML element

--- src/mines/ui/renderMine.ts
+++ src/mines/ui/renderMine.ts
@@ -1,8 +1,4 @@
 export const renderMine = (cell: HTMLButtonElement): void => {
-  const mine = document.createElement("img") as HTMLImageElement;
-
-  mine.innerHTML = `
-  <img src="/media/bomb-icon.svg" alt="deactivated grey bomb with crossed eyes and opened mouth as if exhausted" width="20" height="20">`;
-
-  cell.appendChild(mine);
+  cell.innerHTML =
+    "<img src='/media/bomb-icon.svg' alt='deactivated grey bomb with crossed eyes and opened mouth as if exhausted' width='20' height='20'>";
 };
