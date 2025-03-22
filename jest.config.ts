5f8c06a5d3c723df6691de2672f41f7002576789
Ivet Lopez <ivet19@live.com>
2025-03-22 13:14:04 +0100

    Add Jest configuration to the project

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import { createDefaultPreset, JestConfigWithTsJest } from "ts-jest";
 
 const jestConfig: JestConfigWithTsJest = {
   rootDir: "src",
+  resolver: "ts-jest-resolver",
   verbose: true,
   ...createDefaultPreset(),
 };
